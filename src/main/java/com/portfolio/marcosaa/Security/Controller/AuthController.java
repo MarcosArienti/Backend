a0dd2fcd649ca0a3bf6819ed7ea7604944ccd5ab
MarcosArienti <marcos.arienti311@gmail.com>
2023-05-11 14:10:15 -0300

    Otros cambiossss

--- src/main/java/com/portfolio/marcosaa/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/marcosaa/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping
 @CrossOrigin(origins = "https://frontend-df4d6.web.app")
 public class AuthController {
     @Autowired
@@ -44,7 +44,7 @@ public class AuthController {
     @Autowired
     JwtProvider jwtProvider;
     
-    @PostMapping("/nuevo")
+    @PostMapping("/auth/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Mensaje("Campos mal puestos o email invalido"),HttpStatus.BAD_REQUEST);
@@ -69,7 +69,7 @@ public class AuthController {
         return new ResponseEntity (new Mensaje("Usuario guardado"), HttpStatus.CREATED);
     }
     
-    @PostMapping("/login")
+    @PostMapping("/auth/login")
     public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
       if(bindingResult.hasErrors())
           return new ResponseEntity(new Mensaje("Campos mal puestos"),HttpStatus.BAD_REQUEST);
