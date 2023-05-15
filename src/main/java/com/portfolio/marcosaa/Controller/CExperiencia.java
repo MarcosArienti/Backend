751e78f2eb4fca8b4f67e527b00d350e6b1dfae0
MarcosArienti <marcos.arienti311@gmail.com>
2023-05-15 18:25:18 -0300

    url nueva

--- src/main/java/com/portfolio/marcosaa/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/marcosaa/Controller/CExperiencia.java
@@ -22,8 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://msaa-portfolio.web.app")
+//CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://msaa-portfolio.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
