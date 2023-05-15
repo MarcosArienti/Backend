5dc002e927ff306244d081465f40df573604d8d8
MarcosArienti <marcos.arienti311@gmail.com>
2023-05-15 13:14:12 -0300

    Corrigiendo URL bknd

--- src/main/java/com/portfolio/marcosaa/Controller/PersonaController.java
+++ src/main/java/com/portfolio/marcosaa/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/personas")
 @RestController
-@CrossOrigin(origins = {"https://msaa-portfolio.web.app/","http://localhost:4200"})
+@CrossOrigin(origins = {"https://msaa-portfolio.web.app","http://localhost:4200"})
 public class PersonaController {
     
     @Autowired
